96484fdad3c1869c5883c463e4f424681c969af3
khalidelwadi <khalid.ellwadi@gmail.com>
2023-08-20 17:33:54 +0100

    elevator pitch added

--- script.js
+++ script.js
@@ -2,28 +2,39 @@
 let home = document.getElementById('home')
 let about_me = document.getElementById('about_me')
 let portfolio = document.getElementById('portfolio')
+let elevator = document.getElementById('elevator-pitch')
 
 // select head links
 let home_btn = document.getElementById('home-btn')
 let aboutme_btn = document.getElementById('aboutme-btn')
 let portfolio_btn = document.getElementById('portfolio-btn')
+let elv_btn = document.getElementById('elv-btn')
 
 
 home_btn.addEventListener('click', () => {
+        elevator.style.display = 'none';
         portfolio.style.display = 'none';
         about_me.style.display = 'none';
         home.style.display = 'block';
 })
 
 aboutme_btn.addEventListener('click', () => {
+    elevator.style.display = 'none';
     portfolio.style.display = 'none';
     home.style.display = 'none';
     about_me.style.display = 'block';
 })
 
 portfolio_btn.addEventListener('click', () => {
+    elevator.style.display = 'none';
     home.style.display = 'none';
     about_me.style.display = 'none';
     portfolio.style.display = 'block';
 })
 
+elv_btn.addEventListener('click', () => {
+    home.style.display = 'none';
+    about_me.style.display = 'none';
+    portfolio.style.display = 'none';
+    elevator.style.display = 'block'
+})
